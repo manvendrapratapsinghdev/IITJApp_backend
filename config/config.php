1d5f261cbac4c55a62fcb6676e30988142e34f57
Manvendra Pratap Singh <>
2026-01-21 21:41:58 +0530

    update the code below with a concise commit message describing your changes.

--- config/config.php
+++ config/config.php
@@ -1,17 +1,18 @@
 <?php
 return [
   'db' => [
-    'host' => '127.0.0.1',
-    'dbname' => 'campus_app',
-    'user' => 'root',
-    'pass' => ''
+    // 'host' => '127.0.0.1',
+    // 'dbname' => 'campus_app',
+    // 'user' => 'root',
+    // 'pass' => ''
+    'host' => 'http://13.218.227.172',
+    'dbname' => 'admin_health',
+    'user' => 'appuser',
+    'pass' => 'GyanTech@2025'
   ],
   'secret' => 'dK7f93!aPzR1mQ0sTg9hL5wYb2UvNxZq',
   'super_admin_secret' => 'SA_2025_CampusApp_SecretKey_v1!',
   'super_admin_master_id' => 'CAMPUS_SUPER_ADMIN_2025'
 ];
 
-    // 'host' => 'http://13.218.227.172',
-    // 'dbname' => 'admin_health',
-    // 'user' => 'appuser',
-    // 'pass' => 'GyanTech@2025'
+    
