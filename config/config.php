9cbcf45dd3e133d73b70db43e83b55005bebb6e2
Manvendra Pratap Singh <manvendra.singh@htdigital.in>
2026-01-23 19:05:05 +0530

    delete not required email files and move send_reminders.php to root directory

--- config/config.php
+++ config/config.php
@@ -1,18 +1,24 @@
 <?php
 return [
   'db' => [
-    // 'host' => '127.0.0.1',
-    // 'dbname' => 'campus_app',
-    // 'user' => 'root',
-    // 'pass' => ''
-    'host' => 'http://13.218.227.172',
-    'dbname' => 'admin_health',
-    'user' => 'appuser',
-    'pass' => 'GyanTech@2025'
+    // 'host' => 'localhost',
+    // 'dbname' => 'admin_health',
+    // 'user' => 'appuser',
+    // 'pass' => 'GyanTech@2025'
+
+    'host' => '127.0.0.1',
+    'dbname' => 'campus_app',
+    'user' => 'root',
+    'pass' => ''
   ],
   'secret' => 'dK7f93!aPzR1mQ0sTg9hL5wYb2UvNxZq',
   'super_admin_secret' => 'SA_2025_CampusApp_SecretKey_v1!',
   'super_admin_master_id' => 'CAMPUS_SUPER_ADMIN_2025'
 ];
 
-    
+//  'host' => 'localhost',
+//     'dbname' => 'admin_health',
+//     'user' => 'appuser',
+//     'pass' => 'GyanTech@2025'
+
+   
---
+++ send_reminders.php
@@ -0,0 +1,4 @@
+#!/usr/bin/env php
+<?php
+// Convenience wrapper: allows running from repo root with `php send_reminders.php`
+require __DIR__ . '/email/send_reminders.php';
