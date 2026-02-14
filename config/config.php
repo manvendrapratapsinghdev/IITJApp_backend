c4d8dd85904720483316a5e6b5bc980351266cfe
Manvendra Pratap Singh <manvendra.singh@htdigital.in>
2026-02-14 12:16:47 +0530

    chore: update DB connection config – deploy on 2026-02-14

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 return [
   'db' => [
-    // 'host' => 'localhost',
-    // 'dbname' => 'admin_health',
-    // 'user' => 'appuser',
-    // 'pass' => 'GyanTech@2025'
+    'host' => 'localhost',
+    'dbname' => 'admin_health',
+    'user' => 'appuser',
+    'pass' => 'GyanTech@2025'
 
-    'host' => '127.0.0.1',
-    'dbname' => 'campus_app',
-    'user' => 'root',
-    'pass' => ''
+    // 'host' => '127.0.0.1',
+    // 'dbname' => 'campus_app',
+    // 'user' => 'root',
+    // 'pass' => ''
   ],
   'secret' => 'dK7f93!aPzR1mQ0sTg9hL5wYb2UvNxZq',
   'super_admin_secret' => 'SA_2025_CampusApp_SecretKey_v1!',
